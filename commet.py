4201152692b354d3255bddbb98c982e1362315bb
saiteja6111 <saitejain8055@gmail.com>
2025-05-21 18:00:22 +0530

    python day_2

---
+++ commet.py
@@ -0,0 +1,9 @@
+#these are the python comments represnted by # where these are used to represent the code and explain them
+
+#exe 2-11
+import this
+a  = 10 #assiging 10 to the varable a
+b = 11 #assining 11 to the variable b
+
+print(a + b) #doing addition operation in the program of the two variables a and b
+
---
+++ numbers.py
@@ -0,0 +1,19 @@
+universe_age = 10_000_000_000_000 #we can give spaces to the numbers
+
+print(universe_age)
+
+
+#muliple assigment
+
+a,b,c =1,2,3
+
+#exe 2-9 
+
+print(5+3)
+print(9-1)
+print(4*2)
+print(int(16/2))
+#exe 2-10
+favrioute_number = 12
+
+print(f"your favirote number is {favrioute_number}")
