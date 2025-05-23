e5f97d69005d2b2bab6706241ce1f933c757a3cb
saiteja6111 <saitejain8055@gmail.com>
2025-05-23 11:39:46 +0530

    updated changes

---
+++ motorcycles.py
@@ -0,0 +1,42 @@
+car_brands = ['honda','bmw','pagani','mazada','toyota','ford']
+
+car_brands[0] = 'benz'#changing elements in the list by assigning with indexes
+
+print(car_brands)
+
+car_brands.append('Bugati')#adding elements at the end of the list
+
+print(car_brands)
+
+motor_cycles = []
+
+motor_cycles.append('yamaha')
+
+motor_cycles.append('suzuki')
+
+motor_cycles.append('tvs')
+
+print(motor_cycles)
+
+
+#we can add the elements in the list at a particular position using insert method
+
+fruits = ['apple','banana','pineapple']
+
+print(fruits)
+fruits.insert(1,'grapes')
+
+print(fruits) 
+
+del fruits[2]#use the del statement to remove elements 
+
+print(fruits)
+
+
+#we can remove the items from a list at last using pop method or by indexing
+
+li_1 = []
+
+
+
+
---
+++ python_lists.py
@@ -0,0 +1,36 @@
+bicycles = ['apple','banana','pineapple','orange','grapes']
+
+print(bicycles[3])#accessing list elements by indexing
+
+print(bicycles[0].title())#we can also perform string operations in the list elements if the element is string
+
+
+print(bicycles[-1])
+
+print(f"the fruit name is {bicycles[1]}")
+
+#exe 3-1
+
+names = ["saiteja","koushik","chaitanya","balaji","aakash"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+#exe 3-2
+print(f"ironman is {names[0]}")
+
+print(f"Hulk is {names[1]}")
+
+print(f"Hawkeye name is {names [2]}")
+
+print(f"capitan america name is {names [3]}")
+
+print(f"thor name is {names[4]}")
+
+#exe3-3
+
+list_own = ["my own gaming pc","going to trip","playing video games"]
+
+print(list_own)
